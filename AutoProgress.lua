3a441890c8cbcac6e6a557a5a83f62d8a5ce712b
Zai <107976300+Ceepizz@users.noreply.github.com>
2026-09-25 10:22:48 +0800

    Update AutoProgress.lua

--- AutoProgress.lua
+++ AutoProgress.lua
@@ -12,7 +12,7 @@ local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait()
 
 local LOBBY_PLACE_ID = 3260590327
 
-local LOADING_TIMEOUT = 10
+local LOADING_TIMEOUT = 60
 local LOBBY_LOADING_TIMEOUT = 60
 local GAMEOVER_STEP_TIMEOUT = 10
 local NO_GAMEOVER_TIMEOUT = 35 * 60
